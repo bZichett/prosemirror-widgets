2c3de7f4c4db6484449c29efea45d2491b185d30
Pete Boysen <pboysen@192.168.1.6>
2016-09-26 10:59:38 -0500

    added stability as tool in cloudsim

--- src/sims/cloudsim/main.js
+++ src/sims/cloudsim/main.js
@@ -2,22 +2,29 @@ import {getStore} from "../utils"
 import {Url} from "url"
 
 createjs.MotionGuidePlugin.install()
+let searchParams = new URLSearchParams(window.location.search.substring(1))
 
+let tool = searchParams.get('tool') || "grid"
 
-let store = getStore(), CLOUDS = "clouds"
-let init_clouds = [
+let store = getStore()
+let init_grid_clouds = [
 	"altocumulus","cumulonimbus","cirrocumulus","nimbostratus","cumulus",
 	"cumulonimbus","stratus","nimbostratus","cirrus","cirrostratus",
 	"stratocumulus","altostratus","cumulonimbus","blank","blank"
 ]
+
+let init_stability_clouds = [
+	"altocumulus","blank","stratus","blank","nimbostratus", 
+	"cirrus","cumulonimbus","cirrostratus", "altostratus","blank"
+                    ]
 let clouds = []
 let forms = ["streaks","sheets","heaps","rain","rain"]
 
 function getClouds() {
-	let clouds = store.get(CLOUDS)
+	let clouds = store.get(tool+"_clouds")
 	if (!clouds) {
-		clouds = init_clouds
-		store.set(CLOUDS,clouds)
+		clouds = tool == "grid"? init_grid_clouds: init_stability_clouds
+		store.set(tool+"_clouds",clouds)
 	}
 	return clouds
 }
@@ -28,21 +35,29 @@ function updateClouds() {
 	clouds.forEach(c => {
 		map = map.concat({name:c.name,x:c.x,y:c.y})
 	})
-	map.sort(function compare(a,b) {
-		// y is in reverse order
-		if (a.y > b.y) return 1
-		else if (a.y < b.y) return -1
-		if (a.x > b.x) return 1
-		else if (a.x < b.x) return -1
-		return 0
-	})
+	map.sort(tool == "grid"?
+		function compare(a,b) {
+			// y is in reverse order
+			if (a.y > b.y) return 1
+			if (a.y < b.y) return -1
+			if (a.x > b.x) return 1
+			if (a.x < b.x) return -1
+			return 0
+		}:
+			function compare(a,b) {
+			if (a.y > 200 && b.y < 200) return 1
+			if (a.y < b.y) return -1
+			return 1
+		}
+	)
+			
 	let newclouds = []
 	map.forEach(c => { newclouds = newclouds.concat(c.name) })
-	store.set(CLOUDS,newclouds)
+	store.set(tool+"_clouds",newclouds)
 }
 
 function removeClouds() {
-	store.remove(CLOUDS)
+	store.remove(tool+"_clouds")
 }
 
 class Cloud {
@@ -142,21 +157,81 @@ class Grid {
 	}
 }
 
+class WarmAirBox {
+	constructor(stage,name,xorg,yorg) {
+		let rec = new createjs.Shape()
+		rec.graphics.beginStroke("#000").beginFill("#FFA07A").drawRect(xorg,yorg,580,180).endStroke()
+		let front = new createjs.Shape()
+		front.graphics.beginStroke("#000").beginFill("#D2B48C").drawRect(xorg,yorg+160,580,20).endStroke()
+		let slope = new createjs.Shape()
+		slope.graphics.beginStroke("#000").beginFill("#87CEFA").moveTo(xorg+20,yorg+160).lineTo(xorg+580,yorg+100).lineTo(xorg+580,yorg+160).lineTo(xorg+20,yorg+160).endStroke()
+		let title = new createjs.Text(name,"bold 12px Arial","#00F")
+		title.x = xorg+5
+		title.y = yorg+10
+		let sfront = new createjs.Text("surface front","bold 12px Arial","#000")
+		sfront.x = xorg+25
+		sfront.y = yorg+165
+		let cool = new createjs.Text("cool air","bold 12px Arial","#000")
+		cool.x = xorg+300
+		cool.y = yorg+135
+		stage.addChild(rec,front,slope,title,sfront,cool)
+	}
+}
+
+class Stability {
+	constructor(stage,xorg,yorg) {
+		let top = new WarmAirBox(stage,"Warm Air(stable)",xorg,yorg)
+		let bot = new WarmAirBox(stage,"Warm Air(unstable)",xorg,yorg+200)
+		this.showClouds(stage,xorg,yorg)
+	}
+	
+	showClouds(stage,xorg,yorg) {
+		let x = xorg + 30
+		let y = yorg + 50
+		getClouds().forEach(name => {
+			let c = new Cloud(stage,name,x,y)
+			clouds = clouds.concat(c)
+			stage.addChild(c)
+			x += 103
+			y -= 11
+			if (x > (xorg+500)) {
+				x = xorg + 40
+				y = yorg + 245
+			}
+		})
+	}
+}
+
 class CloudSim {
 	constructor() {
 		this.mainstage = new createjs.Stage("maincanvas")
 		createjs.Touch.enable(this.mainstage)
 		this.mainstage.enableMouseOver()
-		this.grid = new Grid(this.mainstage,50,50)
 		let inst = document.getElementById("instruct")
-		inst.innerHTML = "Rearrange the clouds by dragging and dropping to the correct level and form.<br/>" +
-			"Clouds with multiple copies should be placed in a rain column at the correct levels."
 		let reset = document.getElementById("reset")
-		reset.addEventListener("click", e => {
-			removeClouds()
-			clouds.forEach(c => this.mainstage.removeChild(c))
-			this.grid.showClouds(this.mainstage,50,50)
-		})
+		switch (tool) {
+			case "grid": {
+				this.grid = new Grid(this.mainstage,50,50)
+				inst.innerHTML = "Rearrange the clouds by dragging and dropping to the correct level and form.<br/>" +
+					"Clouds with multiple copies should be placed in a rain column at the correct levels."
+				reset.addEventListener("click", e => {
+					removeClouds()
+					clouds.forEach(c => this.mainstage.removeChild(c))
+					this.grid.showClouds(this.mainstage,50,50)
+				})
+				break;
+			}
+			case "stability": {
+				this.stability = new Stability(this.mainstage,10,10)
+				inst.innerHTML = "Rearrange the clouds by dragging and dropping to the correct stability and altitude."
+				reset.addEventListener("click", e => {
+					removeClouds()
+					clouds.forEach(c => this.mainstage.removeChild(c))
+					this.stability.showClouds(this.mainstage,10,10)
+				})
+				break;
+			}
+		}
 		let dl = document.getElementById("download")
 		dl.addEventListener("click", e => {
 			let dt = this.mainstage.canvas.toDataURL('image/png')
